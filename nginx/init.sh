bdacc8783e58e961c39648f749f53b2e6cab49a8
jimcjzheng <jimcjzheng@tencent.com>
2023-05-05 10:20:58 +0800

    init nginx

--- nginx/init.sh
+++ nginx/init.sh
@@ -2,8 +2,9 @@
 
 # rm -rf ./instance
 
-mkdir -p ./instance/conf ./instance/html ./instance/logs
-# sudo cp -r ./nginx /docker/nginx
-# sudo chmod -R g+rwx /docker/nginx/html
+mkdir -p ./instance/etc/nginx ./instance/usr/share/nginx/html ./instance/log
 
-# docker-compose up
+docker-compose up -d
+
+docker cp `docker ps | grep "nginx" | awk '{print $1}'`:/etc/nginx ./instance/etc
+docker cp `docker ps | grep "nginx" | awk '{print $1}'`:/usr/share/nginx/html/ ./instance/usr/share/nginx/
